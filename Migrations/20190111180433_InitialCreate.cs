bacf30eb3effd3b82fac807877cabcfcd1751447
alirezaseif28 <alirezaseif28@gmail.com>
2019-01-11 21:57:47 +0330

    Init

---
+++ Migrations/20190111180433_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcDemo.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    City = table.Column<string>(nullable: false),
+                    Department = table.Column<string>(nullable: false),
+                    Salary = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ Models/Employees.cs
@@ -0,0 +1,19 @@
+
+using System.ComponentModel.DataAnnotations;
+namespace MvcDemo.Models
+{
+    
+public class Employees  
+    {  
+        [Key]
+        public int Id { get; set; }  
+        [Required]  
+        public string Name { get; set; }  
+        [Required]  
+        public string City { get; set; }  
+        [Required]  
+        public string Department { get; set; }  
+        [Required]  
+        public int Salary {get;set;}  
+    }  
+}
